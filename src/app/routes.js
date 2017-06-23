ee9e8e0726cb4cc4ac9bbd4f71aa170ff3594187
Simon Poole <simon.poole@students.plymouth.ac.uk>
2017-06-23 01:22:21 +0100

    Basic placeholder

--- src/app/routes.js
+++ src/app/routes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* Send home page for all non-image links */
 router.get('/*', function(req, res, next) {
-
+	res.send('Nothing here yet - coming soon!');
 });
 
 module.exports = router;
