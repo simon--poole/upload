da732e7aae118f7437c9ddf449d420f130db5901
Simon Poole <simon.poole@students.plymouth.ac.uk>
2017-06-25 18:30:09 +0100

    Zoho verification

--- src/app/routes.js
+++ src/app/routes.js
@@ -5,13 +5,17 @@ var path = require('path');
 
 router.use('/assets', express.static(path.join(__dirname, '..','assets')));
 
+router.get('/zohoverify/verifyforzoho.html', (req, res, next) => {
+	res.sendFile(path.join(__dirname, '..','assets','verify.html'));
+});
+
 /* Get specific image */
-router.get('/i/:file', function(req, res, next) {
+router.get('/i/:file', (req, res, next) => {
 	res.sendFile(req.params.file, { root: path.join(__dirname, '../../uploads') });
 });
 
 /* Send home page for all non-image links */
-router.get('/*', function(req, res, next) {
+router.get('/*', (req, res, next) => {
 	res.sendFile(path.join(__dirname, '..','assets','index.html'));
 });
 
